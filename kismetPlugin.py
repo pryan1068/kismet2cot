674e1e1609528c5e9e3696f038c9fe60e5bb1aee
Pat Ryan <github@theryanranch.com>
2023-09-19 19:20:56 -0400

    cleaned up filters

--- kismetPlugin.py
+++ kismetPlugin.py
@@ -8,12 +8,32 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 import xml.etree.ElementTree as ET
+import takproto
 
 # Kismet user and password that you assigned the first time you ran it
 USER='user'
 PW='password'
 
+basenameKey="kismet.device.base.name"
+basenameAlias="device.name"
+lastGeopointKey="kismet.device.base.location/kismet.common.location.last/kismet.common.location.geopoint"
+lastGeopointAlias="location.geopoint"
+
+# Very strange: signal only works if I use an underbar vice a period on last node:
+rssiKey="kismet.device.base.signal/kismet.common.signal.last_signal"
+rssiAlias="signal.last.signal"
+
+altKey="kismet.common.location.alt"
+altAlias="alt"
+manufKey="kismet.device.base.manuf"
+manufAlias="manuf"
+ssidKey="dot11.device/dot11.device.advertised.ssid.map/dot11.advertisedssid.ssid"
+ssidAlias="dot11.advertisedssid.ssid"
+macAddrKey="kismet_device_base_macaddr"
+macAddrAlias="macAddr"
+
 class KismetPlugin:
+            
     # Copy data to the cotqueue
     class MyReceiver(pytak.QueueWorker):
         async def run(self):
@@ -29,7 +49,7 @@ class KismetPlugin:
                     r = requests.get(url, auth=basic)
 
                 except Exception as e:
-                    print(e)
+                    # print(e)
                     self._logger.error(f"Connect failed to {url}. Retrying in 3 seconds...")
                     time.sleep(3)
 
@@ -48,17 +68,19 @@ class KismetPlugin:
             # would end up dropping network packets. So only request what you need.
             # See the kismet2cot() function below where this data is used.
             devicesRequest = "ws://localhost:2501/devices/monitor.ws"
+
             req = {
                 "monitor": "*",
                 "request": 1,
                 "rate": 1,
                 "fields": [ 
-                    "kismet.device.base.name",
-                    "kismet.common.location.geopoint",
-                    "kismet.common.location.alt",
-                    "kismet.device.base.manuf",
-                    "dot11.advertisedssid.ssid",
-                    "kismet.common.signal.max_signal"
+                    [basenameKey, basenameAlias],
+                    [lastGeopointKey, lastGeopointAlias],
+                    [altKey, altAlias],
+                    [manufKey, manufAlias],
+                    [ssidKey, ssidAlias],
+                    [rssiKey, rssiAlias],
+                    [macAddrKey, macAddrAlias]
                 ]
             }
 
@@ -82,7 +104,7 @@ class KismetPlugin:
                         # Input the cot data onto the cotqueue
                         await self.config.cotqueue.put(data)
             except websockets.exceptions.InvalidStatusCode as code:
-                print('code=', code.status_code)
+                # print('code=', code.status_code)
                 if code.status_code == 401:
                     print("======================================================================")
                     print(f"Verify username({USER}) and password({PW}) are matching in kismet")
@@ -95,16 +117,30 @@ async def kismet2cot(data):
 
     # NOTE: If you want to use other data from kismet, you need to add those to the filter
     # that is defined in the run() method above.
-    name=get('kismet.device.base.name', obj, "UNK")
-    geopoint=get('kismet.common.location.geopoint', obj, ["0.0", "0.0"])
-    alt=get('kismet.common.location.alt', obj, "0")
-    heading=get('kismet.common.location.heading', obj, "0")
-    manf=get('kismet.device.base.manuf', obj, "UNK")
-    ssid=get('dot11.advertisedssid.ssid', obj, "UNK")
-    rssi=get('kismet.common.signal.max_signal', obj, "0")
+    name=get(basenameAlias, obj, "UNK")
+
+    lat="0"
+    lon="0"
+
+    if lastGeopointAlias in obj:
+        geopoint = obj[lastGeopointAlias]
+        if geopoint==None or geopoint==0:
+            lat = "0.0"
+            lon = "0.0"
+        else:
+            lat = str(geopoint[1])
+            lon = str(geopoint[0])
+
+    alt=get(altAlias, obj, "0")
+    # heading=get(headingAlias, obj, "0")
+    manf=get(manufAlias, obj, "UNK")
+    ssid=get(ssidAlias, obj, "UNK")
+    rssi=get(rssiAlias, obj, "0")
+    macAddr=get(macAddrAlias, obj, "0")
 
     # name="UNK"
-    # geopoint={"0.0", "0.0"}
+    # lat="0.0"
+    # lon="0.0"
     # alt="0"
     # heading="0"
     # manf="UNK"
@@ -113,46 +149,52 @@ async def kismet2cot(data):
 
     event = ET.Element("event")
     event.set("version", "2.0")
-    event.set("type", "t-x-d-d")
-    event.set("uid", ssid)
+    event.set("type", "a-f-u")
+    event.set("uid", name)
     event.set("how", "m-g")
     event.set("time", pytak.cot_time())
     event.set("start", pytak.cot_time())
     event.set("stale", pytak.cot_time(3600))
 
     point = ET.SubElement(event, "point")
-    if len(geopoint) == 1:
-        print("ONE")
-        point.set("lat", "0.0")
-        point.set("lon", "0.0")
-    else:
-        print("TWO")
-        point.set("lat", list(geopoint)[0])
-        point.set("lon", list(geopoint)[1])
+    point.set("lat", lat)
+    point.set("lon", lon)
 
-    point.set("hae", alt) # <<<< THIS NEEDS VALIDATED
-    point.set("ce", "0")
+    point.set("hae", "1") # <<<< THIS NEEDS VALIDATED
+    point.set("ce", "10")
+    point.set("le", "10")
 
     detail = ET.SubElement(event, "detail")
-    emitter = ET.SubElement(detail, "emitter")
-    emitter.set("Emitter", ssid)
-    emitter.set("Manf", manf)
-    emitter.set("RSSI", rssi)
+    contact = ET.SubElement(detail, "contact")
+    contact.set("callsign", "hopper")
+    contact.set("endpoint", "192.168.0.46:4242:tcp")
+
+# <contact callsign='Eliopoli HQ' endpoint='192.168.1.10:4242:tcp'/>
+
+
+    # emitter = ET.SubElement(detail, "emitter")
+    # emitter.set("Emitter", ssid)
+    # emitter.set("Manf", manf)
+    # emitter.set("RSSI", rssi)
+    # emitter.set("Mac", macAddr)
 
     # print(ET.dump(event))
 
-    return ET.tostring(event)
+    buf = takproto.xml2proto(ET.tostring(event))
+
+    return buf
 
 def get(key, obj, default):
     # print('key=', key, ' default=', default, ' obj=', obj)
     if key in obj:
         value = obj[key]
-        if value == None or value == 0 or type(value) == None:
-            value = "UNK"
-        # else:
-            # print("return=", value, ' type=', type(value))
+        if value == None or type(value) == None:
+            value = default
     else:
-        # print("return=", default)
         value = default
 
+    if not isinstance(value, str):
+        value = str(value)
+
+    # print("return=", value)
     return value
