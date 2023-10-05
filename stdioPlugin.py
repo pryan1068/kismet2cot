91dd89d8e24b7f798024d1ee6748999dfb6222c7
Pat Ryan <github@theryanranch.com>
2023-10-05 16:49:55 -0400

    removed enclosing class

--- stdioPlugin.py
+++ stdioPlugin.py
@@ -2,32 +2,59 @@
 import asyncio
 import pytak
 import sys
+from configparser import ConfigParser
 
-class StdioPlugin:
-    # Copy data from the cotqueue
-    class MySender(pytak.QueueWorker):
-        async def handle_data(self, data):
-            # Output the cot data to stdout
-            self._logger.info("stdout:%s", data)
-            await asyncio.sleep(.1)
+# Copy data from the cotqueue
+class StdioSender(pytak.QueueWorker):
+    async def handle_data(self, data):
+        # Output the cot data to stdout
+        self._logger.info("stdout:%s", data)
+        await asyncio.sleep(.1)
 
-        async def run(self, number_of_iterations=-1):
-            while True:
-                # Get the cot data from the cotqueue
-                data = await self.config.cotqueue.get()
+    async def run(self, number_of_iterations=-1):
+        while True:
+            # Get the cot data from the cotqueue
+            data = await self.config.cotqueue.get()
+            await self.handle_data(data)
+
+    async def setup(self):
+        asyncio.create_task(self.run())
+
+# Copy data to the cotqueue
+class StdioReceiver(pytak.QueueWorker):
+    async def handle_data(self, data):
+        self._logger.info("stdin:%s", data)
+
+        # Input the cot data onto the cotqueue
+        await self.config.cotqueue.put(data)
+
+    async def run(self):
+        while True:
+            # Get the input data from stdin
+            for data in sys.stdin:
                 await self.handle_data(data)
+                await asyncio.sleep(.1)
+    
+    async def setup(self):
+        asyncio.create_task(self.run())
 
-    # Copy data to the cotqueue
-    class MyReceiver(pytak.QueueWorker):
-        async def handle_data(self, data):
-            self._logger.info("stdin:%s", data)
+async def main():
+    parser = ConfigParser()
+    parser.read("config.ini")
+    config = parser["mycottool"]
 
-            # Input the cot data onto the cotqueue
-            await self.config.cotqueue.put(data)
+    # create a cotqueue in config for all the plugins to use
+    config.cotqueue = asyncio.Queue()
 
-        async def run(self):
-            while True:
-                # Get the input data from stdin
-                for data in sys.stdin:
-                    await self.handle_data(data)
-                    await asyncio.sleep(.1)
+    # Initializes worker queues and tasks.
+    clitool = pytak.CLITool(config)
+    await clitool.setup()
+
+    stdioReceiver = StdioReceiver(clitool.rx_queue, config)
+    stdioSender = StdioSender(clitool.tx_queue, config)
+
+    await stdioReceiver.setup()
+    await stdioSender.setup()
+                
+if __name__ == "__main__":
+    asyncio.run(main())
