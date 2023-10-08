1f946147f584807c6fab2a21f8929f6435054c1c
Pat Ryan <github@theryanranch.com>
2023-10-08 17:15:26 -0400

    Simplified

--- k2c.py
+++ k2c.py
@@ -4,22 +4,28 @@ import asyncio
 import pytak
 from configparser import ConfigParser
 
-from multicast import Multicast
-from multicastPlugin import MulticastSender, MulticastReceiver
 from stdioPlugin import StdioSender
 from kismetPlugin import KismetReceiver
-# import logging
+import logging
 
+# ======================================================================================
+# Kismet to CoT
+#
+# Subscribe to Kismet device detections, convert them to CoT.
+#
+# The kismet login/password and CoT output (TAKServer or EUD) settings are in config.ini
+#
+# ======================================================================================
 async def main():
     # Enable this to see websockets debug output
-    # logger = logging.getLogger('pytak')
-    # logger.setLevel(logging.DEBUG)
-    # logger.addHandler(logging.StreamHandler())
+    logger = logging.getLogger('pytak.classes')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
 
     # config.ini contains configuration settings
     parser = ConfigParser()
     parser.read("config.ini")
-    config = parser["mycottool"]
+    config = parser["kismet"]
 
     # create a cotqueue in config for all the plugins to use
     config.cotqueue = asyncio.Queue()
@@ -28,23 +34,9 @@ async def main():
     clitool = pytak.CLITool(config)
     await clitool.setup()
 
-    clitool.add_tasks(
-        set([
-            # NOTE: ONLY ENABLE 1 SENDER and 1 RECEIVER
-
-            # RECEIVERS:
-            # PyTAK.MyReceiver(clitool.rx_queue, config),
-            # StdioPlugin.MyReceiver(clitool.rx_queue, config),
-            KismetReceiver(clitool.rx_queue, config),
-            
-            # SENDERS:
-            # PyTAK.MySender(clitool.tx_queue, config),
-            StdioSender(clitool.tx_queue, config),
-            # MulticastSender(clitool.tx_queue, config)
-            ])
-    )
+    # Capture Kismet data and send it out as CoT
+    clitool.add_task(KismetReceiver(clitool.tx_queue, config))
     
-
     await clitool.run()
 
 if __name__ == "__main__":
